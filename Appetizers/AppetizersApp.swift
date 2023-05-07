4582b7fb3cb1d543a3cd189512831c86d1544e0e
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2023-05-07 16:40:07 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Guilherme Golfetto on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
